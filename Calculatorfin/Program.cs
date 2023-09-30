9f2693281dacb25e9cab0282657c5a7ecd8efb9a
Pavel Yanulevich <untitledsources@gmail.com>
2023-09-30 17:34:41 +0300

    Changes

--- Calculatorfin/Program.cs
+++ Calculatorfin/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,14 @@ namespace Calculatorfin
 {
     internal class Program
     {
+        static void OpperationEnd()
+        {
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Для продолжения нажмите ЛЮБУЮ КНОПКУ");
+            Console.ResetColor();
+        }
+
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Blue;
@@ -25,107 +34,96 @@ namespace Calculatorfin
             {
                 Console.Clear();
 
-                Console.WriteLine("Пожалуйста, введите первое число:");
-
-                Console.ForegroundColor = ConsoleColor.Green;
-                double a = Convert.ToDouble(Console.ReadLine());
-                Console.ResetColor();
-
-                Console.WriteLine("Введите знак нужной Вам операции: '+' '-' '*' '/'");
-
-                Console.ForegroundColor = ConsoleColor.Green;
-                string operation = Console.ReadLine();
-                Console.ResetColor();
-
-                Console.WriteLine("Введите второе число:");
-
-                Console.ForegroundColor = ConsoleColor.Green;
-                double b = Convert.ToDouble(Console.ReadLine());
-                Console.ResetColor();
-
-                switch (operation)
+                try
                 {
-                    case "+":
-                        Console.WriteLine();
-                        Console.ForegroundColor = ConsoleColor.DarkYellow;
-                        Console.WriteLine("Итог вычислений " + a + " + " + b + ":");
-                        Console.ResetColor();
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine(a + b);
-                        Console.WriteLine();
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.WriteLine("Для продолжения нажмите ЛЮБУЮ КНОПКУ");
-                        Console.ResetColor();
+                    Console.WriteLine("Пожалуйста, введите первое число:");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    double a = Convert.ToDouble(Console.ReadLine());
+                    Console.ResetColor();
 
-                        break;
+                    Console.WriteLine("Введите знак нужной Вам операции: '+' '-' '*' '/'");
 
-                    case "-":
-                        Console.WriteLine();
-                        Console.ForegroundColor = ConsoleColor.DarkYellow;
-                        Console.WriteLine("Итог вычислений " + a + " - " + b + ":");
-                        Console.ResetColor();
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine(a - b);
-                        Console.WriteLine();
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.WriteLine("Для продолжения нажмите ЛЮБУЮ КНОПКУ");
-                        Console.ResetColor();
-                        break;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    string operation = Console.ReadLine();
+                    Console.ResetColor();
 
-                    case "*":
-                        Console.WriteLine();
-                        Console.ForegroundColor = ConsoleColor.DarkYellow;
-                        Console.WriteLine("Итог вычислений " + a + " * " + b + ":");
-                        Console.ResetColor();
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine(a * b);
-                        Console.WriteLine();
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.WriteLine("Для продолжения нажмите ЛЮБУЮ КНОПКУ");
-                        Console.ResetColor();
-                        break;
+                    Console.WriteLine("Введите второе число:");
 
-                    case "/":
-                        if (b == 0)
-                        {
-                            Console.WriteLine();
-                            Console.ForegroundColor = ConsoleColor.DarkYellow;
-                            Console.WriteLine("Итог вычислений " + a + " / " + b + ":");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    double b = Convert.ToDouble(Console.ReadLine());
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine();
+
+                    switch (operation)
+                    {
+                        case "+":
+                            Console.WriteLine("Итог вычислений " + a + " + " + b + ":");
                             Console.ResetColor();
                             Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine(0);
+                            Console.WriteLine(a + b);
+                            OpperationEnd();
+
+                            break;
+
+                        case "-":
+                            Console.WriteLine("Итог вычислений " + a + " - " + b + ":");
+                            Console.ResetColor();
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine(a - b);
+                            OpperationEnd();
+                            break;
+
+                        case "*":
+                            Console.WriteLine("Итог вычислений " + a + " * " + b + ":");
+                            Console.ResetColor();
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine(a * b);
+                            OpperationEnd();
+                            break;
+
+                        case "/":
+                            if (b == 0)
+                            {
+                                Console.WriteLine("Итог вычислений " + a + " / " + b + ":");
+                                Console.ResetColor();
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine(0);
+                                OpperationEnd();
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("Итог вычислений " + a + " / " + b + ":");
+                                Console.ResetColor();
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine(a / b);
+                                OpperationEnd();
+                            }
+
+                            break;
+
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Извините, такой операции не существует.");
                             Console.WriteLine();
                             Console.ForegroundColor = ConsoleColor.Blue;
                             Console.WriteLine("Для продолжения нажмите ЛЮБУЮ КНОПКУ");
                             Console.ResetColor();
-                        }
-
-                        else
-                        {
-                            Console.WriteLine();
-                            Console.ForegroundColor = ConsoleColor.DarkYellow;
-                            Console.WriteLine("Итог вычислений " + a + " / " + b + ":");
-                            Console.ResetColor();
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine(a / b);
-                            Console.WriteLine();
-                            Console.ForegroundColor = ConsoleColor.Blue;
-                            Console.WriteLine("Для продолжения нажмите ЛЮБУЮ КНОПКУ");
-                            Console.ResetColor();
-                        }
-
-                        break;
-
-                    default:
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Извините, такой операции не существует.");
-                        Console.WriteLine();
-                        Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.WriteLine("Для продолжения нажмите ЛЮБУЮ КНОПКУ");
-                        Console.ResetColor();
-                        break;
+                            break;
+                    }
                 }
-                Console.ReadKey();
+                catch (Exception)
+                {
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Пожалуйста, проверьте корректность введённых данных!");
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Для продолжения нажмите ЛЮБУЮ КНОПКУ");
+                    Console.ResetColor();
+                }
+                 Console.ReadKey();
+
             }
         }
     }
